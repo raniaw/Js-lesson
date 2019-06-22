9fbe8d7ce5a41b7293a4f5ebdef43b5f8c48d1e8
raniaw <raniaw@web.de>
2019-06-22 21:40:38 +0200

    changed

---
+++ lesson10/js/script.js
@@ -0,0 +1,107 @@
+let x = new Array();
+let z = new Array();
+let l = new Array();
+
+let drinkList = ["cola", "beer", "tea", "coffee"];
+
+console.log("Drinkliste: " + drinkList);
+document.getElementById("list").innerHTML = "<b>Drinkliste: </b> <u>" + drinkList + "</u><br><br>";
+
+function delDrink(drink) {
+    let i = -1;
+
+    while (i < drinkList.length - 1) {
+        i++;
+        console.log("i: " + i);
+        if (drinkList[i] == drink) {
+            console.log("\'" + drinkList[i] + "\' wurde von der Liste gelöscht ");
+            document.getElementById("liste").innerHTML = "<b>\'" + drinkList[i] + "\'</b> wurde von der Liste gelöscht <br><br><b>Bleibt nur: </b>";
+
+            continue;
+        }
+
+        x.push(drinkList[drinkList.length - i]);
+        // document.getElementById("rest").innerHTML = x + "<br>"; //coffee,tea,beer
+        console.log(x);
+
+    }
+    x.reverse(); //beer, tea, coffee
+    l.push(x);
+    document.getElementById("rest").innerHTML = l + "<br>";
+    return l;
+}
+
+//
+/**
+ * Diese Funktion prüft, ob elementein Array ist
+ * @param {*} myArray 
+ */
+function isArray(myArray) {
+    return myArray.constructor.toString().indexOf("Array") > -1;
+}
+
+console.log(isArray(z)); //true
+console.log(isArray(l)); //true
+
+function delListe(value, index) {
+    l = delDrink(value);
+    console.log("Console -> delListe(): " + l);
+    return l;
+}
+
+delListe("cola"); //Ausgabe -> "beer", "tea", "coffee"
+z.push(x);
+console.log("X-Liste: " + x); //"beer", "tea", "coffee"
+console.log("Z-length:" + z.length); //1
+console.log((x.indexOf("cola"))); // return -1, weil liste keine "cola" beinhaltet hat
+
+function addDrink(drink, index) {
+    let c = 0;
+    console.log("X-length:" + x.length); //3
+    while (c < x.length) {
+        console.log(c);
+        console.log("addDrink liste: " + x[c]);
+        if (c == index) {
+            console.log("drinkliste: " + x[c]);
+            document.getElementById("index").innerHTML = "<b>\'" + x[c] + "\'</b> hat noch index " + index + " <br>";
+            x.splice(c, 0, drink);
+            console.log("drinkliste:  " + x);
+            document.getElementById("addListe").innerHTML = "<b>\'" + x[c] + "\'</b> wurde in der Liste hinzugefügt, hat jetzt index " + index + " <br>";
+            console.log("DrinkListe:  " + x[c]);
+        }
+        c++;
+    }
+    document.getElementById("ganzeListe").innerHTML = "Die Liste jetzt sieht so <b>\'" + x + "\'</b> aus<br>";
+
+    return x;
+}
+//
+/**
+ * Funktion addiert neue Drink in der Liste auf bestiimte PLatz(index)
+ */
+
+function addListe() {
+    z = (addDrink("water", 2));
+    console.log("neue Liste-> z : " + z);
+}
+
+addListe();
+
+console.log("Liste: " + l);
+console.log("\'x addliste():\'" + l);
+console.log("liste -> l: " + l);
+
+/**
+ * 
+ * let drinklist=["water","tea","coffee","cola","beer"];
+function addDrink(drink,place){
+   let c=-1;
+   while(c<drinklist.length-1){
+       c++;
+       if(c==place){            
+           drinklist.splice(c, 0, drink);
+     }
+   }
+   console.log(drinklist.join());
+}
+ */
---
+++ lesson11/js/script.js
@@ -0,0 +1,105 @@
+let li = [];
+let ul;
+// we create to do list array
+let toDoList = ["Learn Javascript"];
+// the first quistion
+let userAnswer = prompt("Möchten Sie etwas machen oder lernen?");
+
+while (userAnswer != "nein") {
+    if (userAnswer == "aufgaben anzeigen") {
+        var pTag = document.createElement("p");
+        pTag.setAttribute("id", "aufgAnz");
+        document.body.appendChild(pTag);
+        document.getElementById("aufgAnz").innerHTML = "Ganze Aufgaben-Liste: <br> ";
+        for (var i = 0; i < toDoList.length; i++) {
+
+            // var node = document.createElement("LI");
+            // var textnode = document.createTextNode("Water");
+            // node.appendChild(textnode);
+            // document.getElementById("myList").appendChild(node);
+            //document.body.appendChild(li);
+            ul = document.createElement("ul");
+            ul.setAttribute("id", "aufg2");
+            li = document.createElement("li");
+            li.setAttribute("id", "list2" + i);
+            document.body.appendChild(li);
+            document.getElementById("list2" + i).innerHTML = toDoList[i];
+        }
+        //console.log(toDoList);
+    } else if (userAnswer == "neue aufgabe") {
+        var pTag = document.createElement("p");
+        pTag.setAttribute("id", "aufgNeu");
+        document.body.appendChild(pTag);
+        document.getElementById("aufgNeu").innerHTML = "<br>Neue Aufgaben-Liste: <br> ";
+        let newToDo = prompt("Schreiben sie bitte Ihre neue Aufgaben");
+        toDoList.push(newToDo);
+        console.log("Length: " + toDoList.length);
+        ul = document.createElement("ul");
+        ul.setAttribute("id", "aufg1");
+        li = document.createElement("li");
+        // li.setAttribute("id", "list");
+        document.body.appendChild(li);
+        for (var i = 0; i < toDoList.length; i++) {
+            li.setAttribute("id", "list" + i);
+            document.body.appendChild(li, "list" + i);
+            document.getElementById("list" + i).innerHTML = toDoList[i];
+            // document.body.appendChild(li);
+        }
+        console.log(toDoList);
+        // console.log(toDoList.forEach(function(value, index, array) {
+        //     "AufgabenNr.: " + index + " Aufgabe: " + value;
+        // }));
+
+        NodeList.prototype.forEach = Array.prototype.forEach;
+        var li_s = document.querySelectorAll('#list').forEach(function(el) {
+            for (let i = 0; i < toDoList.length; i++) {
+                el.innerHTML = toDoList;
+            }
+        })
+
+        //document.getElementById("list").innerHTML = toDoList;
+
+        // var myNodeList = document.querySelectorAll('li');
+        // forEach(myNodeList, function (index, value) {
+        //     console.log(index, value); // passes index +value back!
+        // });
+        // document.getElementById("list").innerHTML = toDoList.forEach(function (value, index, array) {
+        //     "AufgabenNr.: " + index + " Aufgabe: " + value;
+        // });
+
+    }
+    userAnswer = prompt("noch etwas");
+}
+console.log("app wurde geschlossen!!!");
+
+
+
+// var arr = [];
+// for (var i = 0; i < 10; i++) {
+//     arr.push(prompt("Enter a number"));
+// }
+// console.log(arr);
+
+
+// var grades = [];
+// var i;
+// for (i = 0; i < 10; i++) {
+//     grades.push(Number(prompt("Enter your grades:" + (i + 1), "0-100")));
+// }
+// document.write("Your grades: " + grades);
+
+// NUMBER_OF_INPUTS = 10;
+
+// var i = 0; // Loop iterator
+// var userInput; // Input from user
+// sum = 0; //initialise sum
+
+// // Collect inputs
+// for (i = 0; i < NUMBER_OF_INPUTS; i++) {
+//     userInput = parseInt(prompt('Enter input ' + (i + 1) + ' of ' + NUMBER_OF_INPUTS));
+//     sum += userInput;
+//     sum /= NUMBER_OF_INPUTS;
+// }
+
+// // Output the average
+// alert('Average grade: ' + sum.toFixed(2)); //the .toFixed sets it to 2 decimal places
