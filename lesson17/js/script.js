60bcdd6c45625c0911d339e74d796316ba293c40
raniaw <raniaw@web.de>
2019-06-27 10:52:30 +0200

    changed

--- lesson17/js/script.js
+++ lesson17/js/script.js
@@ -1,8 +1,6 @@
 let body = document.querySelector("body");
 let h1 = document.querySelector("h1");
-body.style.background = "lightgreen";
-h1.style = "color:red";
-h1.style.padding = "10%";
+body.style.background = "#93ffff";
 
 let isBlue = false;
 
@@ -10,27 +8,38 @@ let tag = document.getElementById("firstId");
 
 let liTags = document.getElementsByClassName("list");
 
-let headíngTag = document.getElementsByTagName("h2");
+let h1Tag = document.getElementsByTagName("h1");
+h1Tag[0].style = "color:red";
+h1Tag[0].style.padding = "10%";
+h1Tag[1].classList.add("light");
 
-console.log(headíngTag);
+console.log(h1Tag);
+let par = document.getElementsByTagName("p");
+let par1 = document.getElementsByTagName("p")[0];
+var att = document.createAttribute("id");
+att.value = "par";
+par1.setAttributeNode(att);
+console.log("der text vor der Änderung ist:  " + par1.textContent);
 
-setInterval(changeColor, 5000);
+
+
+//setInterval(changeColor, 5000);
 
 function changeColor() {
     // let time = new TimeRanges();
     if (isBlue == true) {
         body.style.background = colorCreate();
         h1.style.color = colorCreate();
-        headíngTag[0].innerHTML = "Hallo";
-        console.log(headíngTag);
+        h1Tag[0].innerHTML = "Hallo";
+        console.log(h1Tag);
         tag.style.color = colorCreate();
         liTags[0].style.background = colorCreate();
         liTags[1].style.color = colorCreate();
     } else {
         body.style.background = colorCreate();
         h1.style.color = colorCreate();
-        headíngTag[0].innerHTML = "Welt";
-        console.log(headíngTag);
+        h1Tag[0].innerHTML = "Welt";
+        console.log(h1Tag);
         tag.style.color = colorCreate();
         liTags[0].style.background = colorCreate();
         liTags[1].style.color = colorCreate();
@@ -46,4 +55,18 @@ function colorCreate() {
     var colGreen = Math.floor(Math.random() * 256);
     color = "rgb(" + colRed + "," + colBlue + "," + colGreen + ")";
     return color;
+}
+
+function myFunction() {
+
+    var btn = document.getElementsByTagName("BUTTON")[0];
+    var x = btn.attributes[1].value;
+    var y = btn.attributes.getNamedItem("onclick").value;
+    document.getElementById("par").innerHTML = "Der text nach dem Anklicken:<br> Der Button hat <br> eine Attributt mit der Name " + x + " und eine Funktion mit der Name:  " + y;
+    console.log(btn.attributes);
+
+    var att1 = document.createAttribute("class");
+    att1.value = "click";
+    par1.setAttributeNode(att1);
+    console.log("Der Text nach der Änderung: " + par1.textContent);
 }
