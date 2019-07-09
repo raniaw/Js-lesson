989c64493e366c686d8a5bd90371af0370748277
raniaw <raniaw@web.de>
2019-07-09 12:05:19 +0200

    changed

--- lesson29/js/script.js
+++ lesson29/js/script.js
@@ -11,11 +11,37 @@ gLink.style.color = colorCreateDark();
 gLink.style.fontFamily = 'Great Vibes';
 gLink.style.fontSize = "2em";
 
-let bLink = document.addEventListener("click", () => {
-    let imgS = document.querySelector("img");
-    imgS.style.display = "none";
-})
+var bLink = document.querySelector("button");
+var imgS = document.querySelector("img");
 
+bLink.addEventListener("click", () => {
+    imgS.style.display = "none";
+});
+
+var shwBtn = document.getElementById("show");
+shwBtn.addEventListener("click", () => {
+    imgS.style.display = "block";
+});
+
+
+var shwBtn = document.querySelectorAll("button")[2];
+shwBtn.addEventListener("click", () => {
+    if (imgS.style.display == "none") {
+        imgS.style.display = "block";
+
+    } else {
+        imgS.style.display = "none";
+    }
+});
+
+var chngBtn = document.getElementById("change");
+chngBtn.addEventListener("click", () => {
+    if (imgS.getAttribute("src") == "img/java.jpg") {
+        imgS.setAttribute("src", "img/javascript.png");
+    } else {
+        imgS.setAttribute("src", "img/java.jpg");
+    }
+});
 /**
  * the same function as above 
  * 
@@ -25,6 +51,53 @@ let bLink = document.addEventListener("click", () => {
  * })
  * 
  */
+var body = document.getElementsByTagName("body")[0];
+var bgrBtnColor = document.getElementById("bgrBtnColor");
+bgrBtnColor.addEventListener("click", () => {
+    body.style.background = colorCreatePastel();
+});
+var wnd = window;
+var widthW, heightW;
+pSize = document.getElementById("size");
+wnd.addEventListener("resize", () => {
+    body.style.background = colorCreateDark();
+    widthW = wnd.innerWidth;
+    heightW = wnd.innerHeight;
+    pSize.innerHTML = "Window Width = " + widthW + "<br>Wimdow Height = " + heightW;
+    gLink.style.color = colorCreateDark();
+    pSize.style.color = colorCreate();
+    pSize.style.fontFamily = 'Great Vibes';
+    pSize.style.fontSize = "2em";
+    console.log("W: " + widthW + "  h: " + heightW);
+});
+
+
+var btnChangeColor = document.getElementsByTagName("BUTTON")[5];
+var x = btnChangeColor.attributes.item(0).name;
+console.log(x);
+var y = btnChangeColor.attributes.item(0).value;
+var pChange = document.getElementById("btnChange")
+btnChangeColor.addEventListener("click", () => {
+    pChange.innerHTML = y;
+    if (x == "class") {
+        btnChangeColor.style.backgroundColor = colorCreate();
+        btnChangeColor.style.borderRadius = brdRadius() + "%";
+        btnChangeColor.style.boxShadow = "1px 1px 15px 2px " + colorCreateDark();
+        pChange.style.color = colorCreate();
+        pChange.innerHTML = "Button has new class \'" + btnChangeColor.attributes.item(0).value + "\' now";
+    }
+})
+
+function brdRadius() {
+    var grRndmin = Math.ceil(1);
+    //console.log(grRndmin);
+    var grRndmax = Math.floor(50);
+    var grRnd = Math.floor(Math.random() * grRndmax) + grRndmin;
+    //console.log(grRnd);
+    return grRnd;
+}
+
+
 
 /**
  * Funktion für die zufällige dunkle Farbeerstellung 
@@ -70,5 +143,15 @@ function colorCreatePastel() {
     }
     color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
     console.log(color);
+    return color;
+}
+
+
+function colorCreate() {
+    var color;
+    var colRed = Math.floor(Math.random() * 256);
+    var colBlue = Math.floor(Math.random() * 256);
+    var colGreen = Math.floor(Math.random() * 256);
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
     return color;
 }
