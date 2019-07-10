4d177c17be380e50d6da5b64bb7c7174274fb2c4
raniaw <raniaw@web.de>
2019-07-10 10:39:13 +0200

    new file exception validate

---
+++ lesson32/js/script.js
@@ -0,0 +1,32 @@
+function validate() {
+    try {
+        var age = document.getElementById("age").value;
+        var pAge = document.getElementById("pAge");
+        if (age >= 18) {
+            return pAge.innerHTML = "Ok";
+        }
+        if (age < 18)
+            throw pAge.innerHTML = "Isn't ok";
+
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+
+// let val= 0;
+// try {
+//     var val = document.getElementById("id").innerHTML;
+//     console.log(val);
+
+// } catch (err) {
+//     console.log(val);
+//     if (val === undefined) {
+//         console.error("ID is not exist");
+//         console.error("Error name: " + err.name);
+//         console.error("Error message: " + err.message);
+//     }
+// } finally {
+//     console.log("Finally execute always");
+// }
