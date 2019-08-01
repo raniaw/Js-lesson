1309fb6a6b57682b5db2939b6381347389ad47e0
raniaw <raniaw@web.de>
2019-08-01 09:49:43 +0200

    new file

--- lesson49/js/script.js
+++ lesson49/js/script.js
@@ -1,6 +1,6 @@
 var body = document.getElementsByTagName("body")[0];
-var pTxt1 = document.createElement("p");
-var pTxt2 = document.createElement("p");
+var ulArr1 = document.createElement("ul");
+var ulArr2 = document.createElement("ul");
 var pTxt3 = document.createElement("p");
 var pTxt4 = document.createElement("p");
 var pTxt5 = document.createElement("p");
@@ -16,11 +16,6 @@ var pRes4 = document.createElement("p");
 var pRes5 = document.createElement("p");
 
 
-
-pTxt1.style.color = colorCreateDark();
-pTxt1.style.fontWeight = "bold";
-pTxt2.style.color = colorCreateDark();
-pTxt2.style.fontWeight = "bold";
 pTxt3.style.color = colorCreateDark();
 pTxt3.style.fontWeight = "bold";
 pTxt4.style.color = colorCreateDark();
@@ -62,10 +57,10 @@ pRes5.style.fontFamily = 'Great Vibes';
 pRes5.style.fontSize = "1.5em";
 
 
-body.appendChild(pTxt1);
+body.appendChild(ulArr1);
 body.appendChild(pPatt1);
 body.appendChild(pRes1);
-body.appendChild(pTxt2);
+body.appendChild(ulArr2);
 body.appendChild(pPatt2);
 body.appendChild(pRes2);
 body.appendChild(pTxt3);
@@ -78,48 +73,77 @@ body.appendChild(pTxt5);
 body.appendChild(pPatt5);
 body.appendChild(pRes5);
 
-function patt1() {
-    var str = "Visit DCIiiii";
-    var patt = /DCI/i;
-    var result = str.match(patt);
-    pTxt1.innerHTML = "Text: " + str;
-    pPatt1.innerHTML = "pattern: " + patt;
-    pRes1.innerHTML = "Result: " + result;
-}
 
-patt1();
 
-function patt2() {
-    var str = "Hallo Mark";
-    var patt = /a/i;
-    var result = str.match(patt);
-    pTxt2.innerHTML = "Text: " + str;
-    pPatt2.innerHTML = "pattern: " + patt;
-    pRes2.innerHTML = "Result -> first: " + result;
-}
-patt2();
+const companies = [
+    { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+];
 
-function patt3() {
-    var str = "LLLooolli Visit 2010007000$ Schooolllllsss";
-    var patt = /[0-9]+/i;
-    var patt1 = /\d+/i;
-    var result = str.match(patt);
-    pTxt3.innerHTML = "Text: " + str;
-    pPatt3.innerHTML = "pattern: " + patt + "<br> and the same " + patt1 + "<br>'\d\' is -> digit;  \' + \' -> more characters";
-    pRes3.innerHTML = "Result: " + result;
-}
-patt3();
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+/**
+ * Ausgabe NAmens des Companies
+ */
+// for (let i = 0; i < companies.length; i++) {
+//     let li = document.createElement("li");
+//     var txtLi = document.createTextNode(companies[i].name);
+//     li.style.fontSize = "1.8em";
+//     li.style.color = colorCreateDark();
+//     li.appendChild(txtLi);
+//     ulArr1.appendChild(li)
+// }
+/// gleiche  wie oben
+companies.forEach((company) => {
+    let li = document.createElement("li");
+    var txtLi = document.createTextNode(company.name);
+    li.style.fontSize = "1.5em";
+    li.style.color = colorCreateDark();
+    li.appendChild(txtLi);
+    ulArr1.appendChild(li);
+});
 
-function patt4() {
-    var str = "LLLooolli Visit 2010007000$ 447646464€ Schooolllllsss";
-    var patt = /[0-9]+(\$|\€)/i;
-    var patt1 = /\d+(\$|\€)/i;
-    var result = str.match(patt);
-    pTxt4.innerHTML = "Text: " + str;
-    pPatt4.innerHTML = "pattern: " + patt + "<br> and the same " + patt1 + "<br>'\d\' is -> digit";
-    pRes4.innerHTML = "Result: " + result;
-}
-patt4();
+
+/**
+ * Ausgabe ages
+ */
+
+var carDrive = [];
+// for (let i = 0; i < ages.length; i++) {
+//     let li = document.createElement("li");
+//     if (ages[i] >= 21) {
+//         var txtLi = document.createTextNode(ages[i]);
+//         li.style.fontSize = "1.5em";
+//         li.style.color = colorCreateDark();
+//         li.appendChild(txtLi);
+//         ulArr2.appendChild(li);
+//         carDrive.push(ages[i]);
+//     }
+// }
+/// gleiche  wie oben
+ages.forEach((age) => {
+    let li = document.createElement("li");
+    if (age >= 21) {
+        var txtLi = document.createTextNode(age);
+        li.style.fontSize = "1.5em";
+        li.style.color = colorCreateDark();
+        li.appendChild(txtLi);
+        ulArr2.appendChild(li);
+        carDrive.push(age);
+    }
+
+});
+console.log(carDrive);
+
+
+
+
 /**
  * Funktion für die zufällige dunkle Farbeerstellung
  */
@@ -133,9 +157,12 @@ function colorCreateDark() {
     var colGreen = Math.floor(Math.random() * (max - min)) + min;
 
     color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
-    console.log(color);
+    // console.log(color);
     return color;
 }
+
+
+
 /**
  * Funktion für die zufällige helle-(Pastel)Farbeerstellung
  */
@@ -149,6 +176,6 @@ function colorCreatePastel() {
     var colGreen = Math.floor(Math.random() * (max - min)) + min;
 
     color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
-    console.log(color);
+    // console.log(color);
     return color;
 }
