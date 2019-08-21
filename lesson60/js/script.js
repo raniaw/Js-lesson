f9cc4212b467c8dce01c5c71aa2df0c9e5eacf93
raniaw <raniaw@web.de>
2019-08-21 09:17:33 +0200

    new file

---
+++ lesson60/js/script.js
@@ -0,0 +1,44 @@
+var body = document.getElementsByTagName("body")[0];
+
+var h3 = document.createElement("h3");
+
+body.appendChild(h3);
+
+
+
+
+/**
+ * Funktion für die zufällige dunkle Farbeerstellung
+ */
+
+
+function colorCreateDark() {
+    var color;
+    var max = 160;
+    var min = 0;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+    console.log(color);
+    return color;
+}
+/**
+ * Funktion für die zufällige helle-(Pastel)Farbeerstellung
+ */
+
+function colorCreatePastel() {
+    var color;
+    var max = 256;
+    var min = 180;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+    console.log(color);
+    return color;
+}
