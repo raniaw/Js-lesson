45911a5d6a6db6b883775bd30d81bbe8b39cac58
raniaw <raniaw@web.de>
2019-08-28 14:47:49 +0200

    new file

---
+++ lesson68/js/script.js
@@ -0,0 +1,95 @@
+var body = document.getElementsByTagName("body")[0];
+var section = document.querySelectorAll("section");
+var imgS = document.querySelectorAll("img");
+var liS = document.querySelectorAll("li");
+
+console.log(section);
+console.log(imgS);
+console.log(liS);
+
+liS.forEach((item) => {
+    item.addEventListener("click", selectChange);
+});
+
+function selectChange(e) {
+    e.preventDefault();
+    let cl = e.target.classList;
+    if (e.target.classList.length == 0) {
+        removeAllClasses();
+        e.target.className = "selected";
+        if (e.target.className == "selected") {
+            let txt = e.target.textContent;
+            let indImg = findInd(liS, txt);
+            console.log(" ind Img " + indImg);
+            section.forEach((sect) => {
+                sect.classList.remove("active");
+            });
+            section[indImg].className = "active";
+            console.log(section[indImg]);
+            console.log(section);
+        }
+
+    } else {
+        e.target.classList.remove("selected");
+    }
+}
+
+function removeAllClasses() {
+    liS.forEach((list, index) => {
+        list.classList.remove("selected");
+        section[index].classList.remove("active");
+        console.log(section[index]);
+    });
+}
+
+function findInd(value, txt) {
+    let indI;
+    value.forEach((item, index) => {
+        if (value[index].textContent == txt) {
+            indI = index;
+            console.log(indI);
+            console.log(txt + " " + index);
+
+        } else {
+            console.log("nicht gefunden");
+
+        }
+    })
+    console.log(indI)
+    return indI;
+}
+
+/**
+ * Funktion für die zufällige dunkle Farbeerstellung
+ */
+
+function colorCreateDark() {
+    var color;
+    var max = 160;
+    var min = 0;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+    console.log(color);
+    return color;
+}
+/**
+ * Funktion für die zufällige helle-(Pastel)Farbeerstellung
+ */
+
+function colorCreatePastel() {
+    var color;
+    var max = 256;
+    var min = 180;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+    console.log(color);
+    return color;
+}
