7db8a081e1c2aa7a5f5906dc3aa149fc2050487e
raniaw <raniaw@web.de>
2019-09-18 10:26:53 +0200

    with callback and without

--- lesson83/js/script.js
+++ lesson83/js/script.js
@@ -1,24 +1,58 @@
-function checkUser() {
+// the same with callback
+function checkUser(callback) {
     setTimeout(function() {
-        console.log("Checking user...")
+        console.log("Checking user...");
+        callback();
     }, 3000)
 }
 
-function downloadFile() {
+function downloadFile(callback) {
     setTimeout(function() {
-        console.log("Downoloading file...")
+        console.log("Downoloading file...");
+        callback();
     }, 2000)
 }
 
-function closeSesion() {
+function closeSesion(callback) {
     setTimeout(function() {
-        console.log("Closing session...")
+        console.log("Closing session...");
+        callback();
     }, 1000)
 }
 
 function run() {
-    checkUser();
-    downloadFile();
-    closeSesion();
+    checkUser(function() {
+        downloadFile(function() {
+            closeSesion(function() {
+
+            });
+        });
+    });
 }
 run();
+
+// tha same without callback
+// function checkUser() {
+//     setTimeout(function() {
+//         console.log("Checking user...")
+//     }, 3000)
+// }
+
+// function downloadFile() {
+//     setTimeout(function() {
+//         console.log("Downoloading file...")
+//     }, 2000)
+// }
+
+// function closeSesion() {
+//     setTimeout(function() {
+//         console.log("Closing session...")
+//     }, 1000)
+// }
+
+// function run() {
+//     checkUser();
+//     downloadFile();
+//     closeSesion();
+// }
+// run();
